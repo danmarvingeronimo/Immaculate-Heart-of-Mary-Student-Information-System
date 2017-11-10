62826eea02a57c1c68ce224898afcaf051e03560
ruffacaranto <carantoruffa@gmail.com>
2017-11-11 02:53:18 +0800

    Changes

--- TeacherPortal/Teacher.master.cs
+++ TeacherPortal/Teacher.master.cs
@@ -10,8 +10,10 @@ public partial class TeacherPortal_Teacher : System.Web.UI.MasterPage
     protected void Page_Load(object sender, EventArgs e)
     {
         if (Session["teacher_id"] != null && Session["UserType_ID"].ToString() == "5")
-            lblogin.Text = "Welcome,  " + Session["first_name"].ToString() + " " + Session["last_name"].ToString();
+
+            lblogin.Text = "Welcome,  " + Session["teacher_FirstName"].ToString() + " " + Session["teacher_LastName"].ToString();
         else
             Response.Redirect("~/Login.aspx");
+
     }
 }
